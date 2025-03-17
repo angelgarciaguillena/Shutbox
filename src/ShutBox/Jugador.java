26efdb7e4cffe56ca3c38783d299f75590d898d3
ana.montero <ana.montero@106-13.info2>
2025-03-17 10:54:13 +0100

    Method checkTile

--- src/ShutBox/Jugador.java
+++ src/ShutBox/Jugador.java
@@ -3,31 +3,46 @@ package ShutBox;
 import java.util.*;
 
 public class Jugador {
-	
+
 	private int dice1;
 	private int dice2;
 	private int diceAddition;
 	private int points;
-	
-	private List<Integer>tablero = new ArrayList<>();
-	
+
+	private List<Integer> tablero = new ArrayList<>();
+
 	public void fillTable(List<Integer> tablero) {
 		for (int i = 0; i < 12; i++) {
-			tablero.add(i+1);
+			tablero.add(i + 1);
 		}
 	}
-	
+
 	public int throwDice() {
 		Random rand = new Random();
-		
+
 		return rand.nextInt(1, 7);
 	}
-	
+
 	public boolean compareNumbers(int dice1, int dice2) {
 		return this.tablero.contains(dice1 + dice2);
 	}
-	
-	
-	
-	
+
+	/**
+	 * Method to check if the tile is still in the board
+	 * 
+	 * @param numberTile
+	 * @return true or false
+	 */
+	public boolean checkTile(int numberTile) {
+		boolean exists = false;
+
+		for (int n : tablero) {
+			if (n == numberTile) {
+				exists = true;
+			}
+		}
+
+		return exists;
+	}
+
 }
